f85ba867ee03f8b1fd8e09f980f6d711c51b378b
pudongping <276558492@qq.com>
2023-09-17 23:37:07 +0800

    neovim

---
+++ Neovim/.config/nvim/lua/essentials.lua
@@ -0,0 +1,62 @@
+local option = vim.opt
+local buffer = vim.b
+local globals = vim.g
+
+-- Globol Settings --
+option.showmode = false
+option.backspace = { "indent", "eol", "start" }
+option.tabstop = 4
+option.shiftwidth = 4
+option.expandtab = true
+option.shiftround = true
+option.autoindent = true
+option.smartindent = true
+option.number = true
+option.relativenumber = true
+option.wildmenu = true
+-- 关闭搜索结果的高亮
+option.hlsearch = false
+option.ignorecase = true
+option.smartcase = true
+-- 自动补全设置
+option.completeopt = { "menuone", "noselect" }
+option.cursorline = true
+option.termguicolors = true
+option.signcolumn = "yes"
+option.autoread = true
+option.title = true
+option.swapfile = false
+option.backup = false
+option.updatetime = 50
+option.mouse = "a"
+option.undofile = true
+option.undodir = vim.fn.expand('$HOME/.local/share/nvim/undo')
+option.exrc = true
+option.wrap = false
+-- 打开纵向新窗口时，在右边显示
+option.splitright = true
+
+-- Buffer Settings --
+buffer.fileenconding = "utf-8"
+
+-- Global Settings --
+-- 设置 leader 键为空格键
+globals.mapleader = " "
+
+-- Key mappings --
+-- 设置禁止使用方向键
+-- vim.keymap.set({ "n", "i", "v" }, "<Left>", "<Nop>")
+-- vim.keymap.set({ "n", "i", "v" }, "<Right>", "<Nop>")
+-- vim.keymap.set({ "n", "i", "v" }, "<Up>", "<Nop>")
+-- vim.keymap.set({ "n", "i", "v" }, "<Down>", "<Nop>")
+
+vim.keymap.set("n", "<A-Tab>", "<cmd>bNext<CR>")
+-- 关闭 buffer
+vim.keymap.set("n", "<leader>bc", "<cmd>bd<CR>")
+
+-- 按大写 J K 在视图模式下可以移动整块
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- 复制内容到系统剪切板
+vim.keymap.set({ "v", "n" }, "<leader>y", "\"+y")
---
+++ Neovim/README.md
@@ -0,0 +1,80 @@
+# [Neovim](https://github.com/neovim/neovim)
+
+- 插件管理器：[folke/lazy.nvim](https://github.com/folke/lazy.nvim)
+
+## 使用 homebrew 安装 neovim
+
+```bash
+# https://formulae.brew.sh/formula/neovim
+brew install neovim
+```
+
+## 安装 git
+
+```bash
+brew install git
+```
+
+## 安装 fzf
+
+```bash
+brew install fzf
+```
+
+## 安装 iTerm2
+
+```bash
+brew install iterm2
+```
+
+## 安装字体 [nerd-fonts](https://github.com/ryanoasis/nerd-fonts#font-installation)
+
+> 方便显示一些小图标。
+
+```bash
+brew tap homebrew/cask-fonts
+brew install font-hack-nerd-font
+```
+
+查看安装后的字体
+
+```bash
+cd ~/Library/Fonts && ll
+```
+
+对 iTerm2 设置字体。需要勾选 **Use a different font for non-ASCII text** 然后在 **Non-ASCII Font** 下选择 `Hack Nerd Font Mono` 字体。
+可以参考下图所示。
+
+![](./imgs/iterm2-setting-front.png)
+
+## 使用配置
+
+```bash
+# 直接通过建立软连接的方式使用（要使用绝对路径的方式）
+# eg：
+ln -s /Users/pudongping/glory/codes/code/home-profile/Neovim/.config/nvim $HOME/.config/nvim
+# 或者使用如下方式，效果一样
+ln -s ~/glory/codes/code/home-profile/Neovim/.config/nvim/ ~/.config/nvim
+
+# 查看建立关系
+ls -al $HOME/.config | grep nvim
+
+# 如果需要删除软连接的话（不直接使用 rm -rf $HOME/.config/nvim 是担心误删）
+cd $HOME/.config && rm -rf nvim
+```
+
+## 小技巧
+
+- 选择某个硬盘路径，按键盘 `gf` 两个字母时，可以直接使用 nvim 直接打开。然后可以通过 `Ctrl+o` 回退回来。光标对着网址路径，按键盘 `gx` 两个字母时，可以直接使用浏览器打开。
+
+## 用到的插件
+
+- [folke/tokyonight.nvim](https://github.com/folke/tokyonight.nvim) 皮肤插件
+
+## 常用指令
+
+```bash
+:Explore 列出当前目录下所有的文件
+:e filename 以 buffer 的形式打开新文件
+
+```
