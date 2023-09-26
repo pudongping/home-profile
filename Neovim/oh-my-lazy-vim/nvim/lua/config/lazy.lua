98d7306aa03500baf58584aef0b1c56c4301f01b
pudongping <276558492@qq.com>
2023-09-27 02:37:06 +0800

    开启 golang lsp

--- Neovim/oh-my-lazy-vim/nvim/lua/config/lazy.lua
+++ Neovim/oh-my-lazy-vim/nvim/lua/config/lazy.lua
@@ -14,6 +14,9 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.docker" },
     -- import/override with your plugins
     { import = "plugins" },
   },
