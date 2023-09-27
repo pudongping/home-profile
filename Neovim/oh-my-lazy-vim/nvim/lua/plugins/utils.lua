d3b5034bc801a629bf43170157768ce79e26eac3
Alex Pu <276558492@qq.com>
2023-09-27 18:57:22 +0800

    去除掉不需要的插件

--- Neovim/oh-my-lazy-vim/nvim/lua/plugins/utils.lua
+++ Neovim/oh-my-lazy-vim/nvim/lua/plugins/utils.lua
@@ -8,4 +8,23 @@ return {
             vim.api.nvim_set_keymap("n", "k", "<Plug>(accelerated_jk_gk)", {})
         end,
     },
+
+    -- auto-resize windows
+    {
+        "anuvyklack/windows.nvim",
+        event = "WinNew",
+        dependencies = {
+            { "anuvyklack/middleclass" },
+            { "anuvyklack/animation.nvim", enabled = false },
+        },
+        keys = { { "<leader>m", "<cmd>WindowsMaximize<cr>", desc = "Zoom" } }, -- 分屏时，可以直接将某个窗口以百叶窗的形式放到最大
+        config = function()
+            vim.o.winwidth = 5
+            vim.o.equalalways = false
+            require("windows").setup({
+                animation = { enable = false, duration = 150 },
+            })
+        end,
+    },
+
 }
